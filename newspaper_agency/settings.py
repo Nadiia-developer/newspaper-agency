e02807374eff22a927b1ae3913d72e787b36a56f
Nadiia <nadiia.developer777@gmail.com>
2023-12-07 15:10:55 +0200

    Update settings.py and requirements.txt

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,16 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4_^heg_a+1t$ro%bxv4krf5@)-)k_(&5o+c*#79&v@%ug8q%!b"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-4_^heg_a+1t$ro%bxv4krf5@)-)k_(&5o+c*#79&v@%ug8q%!b")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
 
 
 # Application definition
@@ -111,6 +108,10 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = "newspaper.Redactor"
 
 LOGIN_REDIRECT_URL = "/"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 
 # Internationalization
