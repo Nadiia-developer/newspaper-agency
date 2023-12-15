70220a96e1540ce6f228fb00789cf165b4700b40
Nadiia <nadiia.developer777@gmail.com>
2023-12-15 20:33:08 +0200

    Update settings.py

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -21,15 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-4_^heg_a+1t$ro%bxv4krf5@)-)k_(&5o+c*#79&v@%ug8q%!b")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 DEBUG = False
 
-# ALLOWED_HOSTS = ["127.0.0.1", "newspaper-agency-co9w.onrender.com"]
 ALLOWED_HOSTS = ["127.0.0.1"]
 
 
@@ -94,7 +91,7 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
-# DATABASE_URL = "postgres://rdsdkbgo:VoGPrvbuf55_F9_WlancTpgSOy3lDqUe@cornelius.db.elephantsql.com/rdsdkbgo"
+
 
 
 # Password validation
